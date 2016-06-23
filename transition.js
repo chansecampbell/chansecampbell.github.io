406e86e6d4a23214c3f4371c738463456f87c2a3
chansecampbell <chansecampbell@gmail.com>
2016-06-23 14:48:11 +0100

    Took out the fadeout for now.

--- transition.js
+++ transition.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
- $("section").css("display", "none");
+// $(document).ready(function() {
+//  $("section").css("display", "none");
 
 
- $("section").fadeIn(1000);
+//  $("section").fadeIn(1000);
 
 
- $("a.transition").click(function(event){
- event.preventDefault();
- linkLocation = this.href;
- $("section").fadeOut(600, redirectPage);
- });
+//  $("a.transition").click(function(event){
+//  event.preventDefault();
+//  linkLocation = this.href;
+//  $("section").fadeOut(600, redirectPage);
+//  });
 
- function redirectPage() {
- window.location = linkLocation;
- }
-});
+//  function redirectPage() {
+//  window.location = linkLocation;
+//  }
+// });
